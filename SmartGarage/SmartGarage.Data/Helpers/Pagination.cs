f5f3b25ad5c49a33a8e6b857b773b97f63c612fb
Todor Vasilev <todor_vasilev14@abv.bg>
2021-05-16 13:46:49 +0300

    Add Pagination setup

---
+++ SmartGarage/SmartGarage.Data/Helpers/Pagination.cs
@@ -0,0 +1,22 @@
+﻿using SmartGarage.Data.QueryObjects;
+using System.Collections.Generic;
+
+namespace SmartGarage.Data.Helpers
+{
+    public class Pager<T>
+    {
+        public Pager(List<T> itemsColection, PaginationQueryObject pagination)
+        {
+            this.ItemsColection = itemsColection;
+            this.ItemsOnPage = itemsColection.Count;
+            this.Page = pagination.Page;
+        }
+        public int Count { get; set; }
+
+        public int Page { get; set; }
+
+        public int ItemsOnPage { get; set; }
+
+        public List<T> ItemsColection { get; set; }
+    }
+}
---
+++ SmartGarage/SmartGarage.Data/QueryObjects/PaginationQueryObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartGarage.Data.QueryObjects
+{
+    public class PaginationQueryObject
+    {
+        public PaginationQueryObject()
+        {
+            this.Page = 1;
+            this.ItemsOnPage = 5;
+        }
+
+        public PaginationQueryObject(int page, int itemsOnPage)
+        {
+            this.Page = page;
+            this.ItemsOnPage = itemsOnPage;
+        }
+
+        public int Page { get; set; }
+
+        public int ItemsOnPage { get; set; }
+    }
+}
