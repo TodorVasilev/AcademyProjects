6ad9f1088084ed33d3d1054fbc0564d9b82b178a
Plamen Velikov <plamen.velikov12@gmail.com>
2021-06-08 22:27:17 +0300

    GetAllServices Linked To Customer Tests GetAllServices  tests GetAllorders tests

--- SmartGarage/SmartGarage.Test/ServiceTests/ServiceServiceTests/GetAllServices_Should.cs
+++ SmartGarage/SmartGarage.Test/ServiceTests/ServiceServiceTests/GetAllServices_Should.cs
@@ -37,23 +37,5 @@ namespace SmartGarage.Test.ServiceTests.ServiceServiceTests
                 Assert.IsInstanceOfType(result[0], typeof(GetServiceDTO));
             }
         }
-
-        [TestMethod]
-        public async Task ReturnNull_When_ThereAreNoServices()
-        {
-            //Arrange
-            var options = Util.GetOptions(nameof(ReturnNull_When_ThereAreNoServices));
-            var filterObject = new ServiceFilterQueryObject();
-
-            //Act
-            using (var actCtx = new SmartGarageContext(options))
-            {
-                var sut = new ServiceService(actCtx);
-                var result = await sut.GetAll(filterObject);
-
-                //Assert
-                Assert.IsNull(result);
-            }
-        }
     }
 }
