7cb6401a59f19539b913fbbb7da623cad09a15c6
Plamen Velikov <plamen.velikov12@gmail.com>
2021-06-08 23:17:09 +0300

    AddServiceToOrder service Test DeleteServiceToOrder service Test

---
+++ SmartGarage/SmartGarage.Test/ServiceTests/OrderServiceTests/DeleteService_Should.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SmartGarage.Data;
+using SmartGarage.Data.Models;
+using SmartGarage.Service;
+using SmartGarage.Service.Contracts;
+using SmartGarage.Service.ServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartGarage.Test.ServiceTests.OrderServiceTests
+{
+	[TestClass]
+	public class DeleteService_Should
+	{
+		[TestMethod]
+		public async Task DeleteServiceFromOrder()
+		{
+			//Arrange
+			var options = Util.GetOptions(nameof(DeleteServiceFromOrder));
+			var currencyServiceFake = new Mock<ICurrencyService>();
+			var userHelperMock = new Mock<IUserHelper>();
+			var vehicleServiceMock = new Mock<IVehicleService>();
+			var emailServiceMock = new Mock<IEmailsService>();
+
+			var serviceOrder = new ServiceOrder()
+			{
+				OrderId = 1,
+				ServiceId = 1
+			};
+
+			using (var arrCtx = new SmartGarageContext(options))
+			{
+				arrCtx.SeedData();
+				await arrCtx.SaveChangesAsync();
+			}
+
+			//Act
+			using (var actCtx = new SmartGarageContext(options))
+			{
+
+				var sut = new OrderService(actCtx,
+					currencyServiceFake.Object,
+					userHelperMock.Object,
+					vehicleServiceMock.Object,
+					emailServiceMock.Object);
+
+					
+				var result = await sut.DeleteService(serviceOrder);
+				var orderCountAfterAdd = actCtx.ServiceOrders.Count(o => o.OrderId == 1);
+
+				//Assert
+				Assert.AreEqual(orderCountAfterAdd, 0);
+				Assert.IsTrue(result);
+			}
+		}
+		[TestMethod]
+		public async Task ReturnFalse_WhenOrder_InOrderServiceIsZero_V2()
+		{
+			//Arrange
+			var options = Util.GetOptions(nameof(ReturnFalse_WhenOrder_InOrderServiceIsZero_V2));
+			var currencyServiceFake = new Mock<ICurrencyService>();
+			var userHelperMock = new Mock<IUserHelper>();
+			var vehicleServiceMock = new Mock<IVehicleService>();
+			var emailServiceMock = new Mock<IEmailsService>();
+
+			var serviceOrder = new ServiceOrder()
+			{
+				OrderId = 0,
+				ServiceId = 3
+			};
+
+			using (var arrCtx = new SmartGarageContext(options))
+			{
+				arrCtx.SeedData();
+				await arrCtx.SaveChangesAsync();
+			}
+
+			//Act
+			using (var actCtx = new SmartGarageContext(options))
+			{
+
+				var sut = new OrderService(actCtx,
+					currencyServiceFake.Object,
+					userHelperMock.Object,
+					vehicleServiceMock.Object,
+					emailServiceMock.Object);
+									
+				var result = await sut.DeleteService(serviceOrder);
+				
+				//Assert
+				Assert.IsFalse(result);
+			}
+		}
+
+		[TestMethod]
+		public async Task ReturnFalse_WhenService_InOrderServiceIsZero_V2()
+		{
+			//Arrange
+			var options = Util.GetOptions(nameof(ReturnFalse_WhenService_InOrderServiceIsZero_V2));
+			var currencyServiceFake = new Mock<ICurrencyService>();
+			var userHelperMock = new Mock<IUserHelper>();
+			var vehicleServiceMock = new Mock<IVehicleService>();
+			var emailServiceMock = new Mock<IEmailsService>();
+
+			var serviceOrder = new ServiceOrder()
+			{
+				OrderId = 1,
+				ServiceId = 0
+			};
+
+			using (var arrCtx = new SmartGarageContext(options))
+			{
+				arrCtx.SeedData();
+				await arrCtx.SaveChangesAsync();
+			}
+
+			//Act
+			using (var actCtx = new SmartGarageContext(options))
+			{
+
+				var sut = new OrderService(actCtx,
+					currencyServiceFake.Object,
+					userHelperMock.Object,
+					vehicleServiceMock.Object,
+					emailServiceMock.Object);
+
+				var result = await sut.DeleteService(serviceOrder);
+
+				//Assert
+				Assert.IsFalse(result);
+			}
+		}
+	}
+}
---
+++ SmartGarage/SmartGarage.Test/ServiceTests/ServiceServiceTests/GetAvailableServices_Should.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartGarage.Data;
+using SmartGarage.Service;
+using SmartGarage.Service.DTOs.GetDTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartGarage.Test.ServiceTests.ServiceServiceTests
+{
+	[TestClass]
+	public class GetAvailableServices_Should
+	{
+        [TestMethod]
+        public async Task ReturnCorrectServic_ForSpecificOrder()
+        {
+            //Arrange
+            var options = Util.GetOptions(nameof(ReturnCorrectServic_ForSpecificOrder));
+            var order = 1;
+            var service = new Data.Models.Service();
+
+            using (var arrCtx = new SmartGarageContext(options))
+            {
+                arrCtx.SeedData();
+                await arrCtx.SaveChangesAsync();
+
+            }
+
+            //Act
+            using (var actCtx = new SmartGarageContext(options))
+            {
+                var sut = new ServiceService(actCtx);
+                var result = await sut.GetAvailableServices(order);
+
+                //Assert
+                Assert.AreEqual(5, result.Count);
+                Assert.IsInstanceOfType(result, typeof(List<GetServiceDTO>));
+            }
+        }
+    }
+}
