fb48ce524e8bd7f71eb4b90c9b46064d68cae19e
Plamen Velikov <plamen.velikov12@gmail.com>
2021-06-09 15:00:05 +0300

    Index Customer service script

---
+++ SmartGarage/SmartGarage/wwwroot/js/Custom_scripts/IndexCustomer_Service.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(function () {
+	$(document).on("click", "#search", function () {
+		var number = $('#numberPlate').val();
+		var date = $('#date').val();
+		$.ajax({
+			url: "/Service/SearchCustomer",
+			type: "GET",
+			data: {
+				numberPlate: number,
+				date: date
+			}
+		})
+			.done(function (partialViewResult) {
+				$("#partialView").html(partialViewResult);
+			});
+	});
+	$(document).on("click", "#next", function () {
+		var number = $('#numberPlate').val();
+		var date = $('#date').val();
+		var page = $('#page').val();
+		$.ajax({
+			url: "/Service/SearchCustomer",
+			type: "GET",
+			data: {
+				numberPlate: number,
+				date: date,
+				pageNumber: parseInt(page) + 1
+			}
+		})
+			.done(function (partialViewResult) {
+				$("#partialView").html(partialViewResult);
+			});
+	});
+	$(document).on("click", "#prev", function () {
+		var number = $('#numberPlate').val();
+		var date = $('#date').val();
+		var page = $('#page').val();
+		$.ajax({
+			url: "/Service/SearchCustomer",
+			type: "GET",
+			data: {
+				numberPlate: number,
+				date: date,
+				pageNumber: parseInt(page) - 1
+			}
+		})
+			.done(function (partialViewResult) {
+				$("#partialView").html(partialViewResult);
+			});
+	});
+});
